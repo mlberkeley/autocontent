d63a93e7757c5f668fa73befab2237999d8ce213
Michael Equi <michaelequi@berkeley.edu>
2023-04-23 18:06:38 -0700

    Added ability for transcribe.py to processes mp4 files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # autocontent
 Tools for automatically generating content from ML@B functions including reading group, vision meetings, and more
+
+## Setup 
+
+`pip install yt-dlp pydub openai moviepy`
--- transcribe.py
+++ transcribe.py
@@ -4,10 +4,22 @@ import yt_dlp
 import openai
 import io
 import math
+import re
 from pydub import AudioSegment
+from moviepy.editor import VideoFileClip
+
 
 # Get the OpenAI API key from an environment variable
 openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+# Check if the input is a YouTube URL
+def is_youtube_url(url):
+    youtube_regex = re.compile(
+        r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+
+    return bool(youtube_regex.match(url))
 
 # Download the YouTube video's audio
 def download_audio(video_url, filename):
@@ -22,6 +34,12 @@ def download_audio(video_url, filename):
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
+
+# Extract audio from local video file
+def extract_audio_from_video(video_file, output_file):
+    video = VideoFileClip(video_file)
+    audio = video.audio
+    audio.write_audiofile(output_file)
 
 # Convert the audio to the required format (WAV, 16kHz, mono)
 def convert_audio(input_file, output_file, start=0, end=0):
@@ -56,30 +74,38 @@ AudioSegment.ffmpeg = "/path/to/ffmpeg"
 AudioSegment.MAX_MEMORY_FOR_PIL = 800 * 1024 * 1024  # 800 MB
 
 # Define the argument parser
-parser = argparse.ArgumentParser(description="Transcribe a YouTube video using the OpenAI Whisper ASR API.")
-parser.add_argument("video_url", help="The YouTube video URL to transcribe.")
+parser = argparse.ArgumentParser(description="Transcribe a YouTube video or local MP4 file using the OpenAI Whisper ASR API.")
+parser.add_argument("input", help="The YouTube video URL or local MP4 file to transcribe.")
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 # Define temporary file names
-downloaded_audio = "downloaded_audio.wav"
+input_audio = "input_audio.wav"
 converted_audio = "converted_audio.wav"
 
-# Download and convert the audio
-download_audio(args.video_url, downloaded_audio)
+# Check if the input is a YouTube URL or a local MP4 file
+if is_youtube_url(args.input):
+    # Download and convert the audio from YouTube video
+    download_audio(args.input, input_audio)
+else:
+    # Extract and convert the audio from local MP4 file
+    extract_audio_from_video(args.input, input_audio)
 
 # Get the total duration of the audio
-audio = AudioSegment.from_wav(downloaded_audio)
+audio = AudioSegment.from_wav(input_audio)
 duration = audio.duration_seconds
 
 num_segments = math.ceil(duration / 600)
+# Remove transcript.txt if it already exists
+if os.path.exists("transcript.txt"):
+    os.remove("transcript.txt")
 for i in range(num_segments):
     start = i * 600
     end = (i + 1) * 600
     if end > duration:
         end = duration
-    convert_audio(downloaded_audio, converted_audio, start=start, end=end)
+    convert_audio(input_audio, converted_audio, start=start, end=end)
     transcript = transcribe_audio(converted_audio)
-    with open("transcript.txt", "a") as f:
+    with open("transcript.txt", "a+") as f:
         f.write(transcript)
